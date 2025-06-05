2b6b2a2fb71f2e5cffae0da61620c03a1a976909
Edoardo Lova <edoardolova81@gmail.com>
2025-06-05 15:27:20 +0200

    add lamp turn on

---
+++ main.js
@@ -0,0 +1,4 @@
+let lampImgEl = document.querySelector(".lamp-img");
+let btnLampEl = document.getElementById("btn-lamp");
+
+btnLampEl.addEventListener('click', () => lampImgEl.src = "./img/yellow_lamp.png")
